58821d498f5915ae3455aeacdbe8c9b8939cfc00
Francois Campbell <campbell.francois@gmail.com>
2013-03-18 19:50:50 -0400

    Revert "Added simple widget"
    This reverts commit 263fbee5c1b6bc747be66fb7eecccde876a4c871.

--- src/ca/carleton/ccsl/cubalance/CUBalanceWidgetProvider.java
+++ src/ca/carleton/ccsl/cubalance/CUBalanceWidgetProvider.java
@@ -17,34 +17,34 @@ import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 
 @SuppressWarnings("unused")
-public class CUBalanceWidgetProvider extends AppWidgetProvider {
-	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		super.onUpdate(context, appWidgetManager, appWidgetIds);
+public class CUBalanceWidgetProvider extends AppWidgetProvider 
+{
+  public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) 
+  {
+    super.onUpdate(context, appWidgetManager, appWidgetIds);
 
-		final int N = appWidgetIds.length;
+    final int N = appWidgetIds.length;
 
-		// Perform this loop procedure for each App Widget that belongs to this provider
-		for (int i = 0; i < N; i++) {
-			int appWidgetId = appWidgetIds[i];
+    // Perform this loop procedure for each App Widget that belongs to this provider
+    for (int i = 0; i < N; i ++) 
+    {
+      int appWidgetId = appWidgetIds[i];
 
-			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.cubalance_widget);
+      RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.cubalance_widget);
+            
+      final SharedPreferences settings 
+        = context.getSharedPreferences(CUBalanceSettings.PREFS_NAME, Activity.MODE_PRIVATE);
 
-			final SharedPreferences settings = context.getSharedPreferences(CUBalanceSettings.PREFS_NAME, Activity.MODE_PRIVATE);
-
-			String lastUp = settings.getString(CUBalanceSettings.DATE_KEY, context.getResources().getString(R.string.unknown_last_up));
-			views.setTextViewText(R.id.tvWidgetLastUp, lastUp);
-
-			String lastBal = settings.getString(CUBalanceSettings.BAL_KEY, context.getResources().getString(R.string.unknown_bal_text));
-			views.setTextViewText(R.id.tvWidgetBalance, lastBal);
-
-			Intent intent = new Intent(context, CUCampusCardBalanceActivity.class);
-			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-			views.setOnClickPendingIntent(R.id.iWidgetLogo, pendingIntent);
-
-			// views.setOnClickPendingIntent(R.id.widgetUpdateButton, pendingIntent);
-
-			appWidgetManager.updateAppWidget(appWidgetId, views);
-		}
-	}
+      //String lastUp    = settings.getString(CUBalanceSettings.DATE_KEY, "");      
+      String lastBal  = settings.getString(CUBalanceSettings.BAL_KEY,  "");
+      views.setTextViewText(R.id.widgetBalanceTxt, lastBal);
+      
+      Intent intent = new Intent(context, CUCampusCardBalanceActivity.class);
+      PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+      
+      //views.setOnClickPendingIntent(R.id.widgetUpdateButton, pendingIntent);
+      
+      appWidgetManager.updateAppWidget(appWidgetId, views);
+    }    
+  }
 }
--- src/ca/carleton/ccsl/cubalance/CUCampusCardBalanceActivity.java
+++ src/ca/carleton/ccsl/cubalance/CUCampusCardBalanceActivity.java
@@ -5,8 +5,6 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 import android.app.Activity;
-import android.appwidget.AppWidgetManager;
-import android.content.ComponentName;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -16,142 +14,144 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RemoteViews;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CUCampusCardBalanceActivity extends Activity {
-	private final String TAG = getClass().getSimpleName();
-	private final DateFormat DATE_FMT = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.SHORT);
-	private final Locale LOC = new Locale("en_US");
-	private final NumberFormat CASH_FMT = NumberFormat.getCurrencyInstance(LOC);
+public class CUCampusCardBalanceActivity extends Activity
+{
+  private final String       TAG      = getClass().getSimpleName();
+  private final DateFormat   DATE_FMT = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.SHORT);
+  private final Locale       LOC      = new Locale("en_US");
+  private final NumberFormat CASH_FMT = NumberFormat.getCurrencyInstance(LOC);
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    
+    final Button button = (Button) findViewById(R.id.updateBalanceBtn);
+    button.setOnClickListener(new View.OnClickListener() 
+    {
+      public void onClick(View v)
+      {
+        fetchBalance();
+      }
+    });
+  }
+  
+  @Override
+  public void onStart()
+  {
+    super.onStart();
+    
+    final SharedPreferences settings 
+      = getSharedPreferences(CUBalanceSettings.PREFS_NAME, MODE_PRIVATE);
+    
+    final TextView balance   = (TextView) findViewById(R.id.balanceTxt);
+    final TextView updatedAt = (TextView) findViewById(R.id.updatedAtTxt);
 
-		final Button button = (Button) findViewById(R.id.updateBalanceBtn);
-		button.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				fetchBalance();
-			}
-		});
-	}
+    Boolean autoUp   = settings.getBoolean(CUBalanceSettings.UPDATE_KEY, false);
+    String prefsUser = settings.getString(CUBalanceSettings.USER_KEY, "");
+    String prefsPin  = settings.getString(CUBalanceSettings.PIN_KEY,  "");
+    String lastBal   = settings.getString(CUBalanceSettings.BAL_KEY,  "");
+    String lastUp    = settings.getString(CUBalanceSettings.DATE_KEY, "");
+    
+    //If the user or pin haven't been changed from the default, we need to show
+    //the settings screen before anything meaningful can happen in this Activity
+    if(prefsUser.equals("") || prefsPin.equals(""))
+    {
+      Log.i(TAG, "No first setup done. Launching CUBalanceSettings activity.");
+      Intent myIntent = new Intent(getBaseContext(), CUBalanceSettings.class);
+      startActivityForResult(myIntent, 0);
+    }
+    
+    //There is a 'cached' balance, set the text box to that value.
+    if(!lastBal.equals(""))
+      balance.setText(lastBal);
+    
+    if(!lastUp.equals(""))
+      updatedAt.setText("Last Updated: "+ lastUp);
+    
+    if(autoUp)
+      fetchBalance();
+  }
+  
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) 
+  {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.menu, menu);
+    return true;
+  }
 
-	@Override
-	public void onStart() {
-		super.onStart();
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) 
+  {
+    // Handle item selection
+    switch (item.getItemId()) 
+    {
+      case R.id.menu_settings_item:
+        Intent myIntent = new Intent(getBaseContext(), CUBalanceSettings.class);
+        startActivityForResult(myIntent, 0);
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+  
+  private void fetchBalance()
+  {
+    final CUCampusCardBalanceActivity mainUI = this;
+    
+    final SharedPreferences settings 
+      = getSharedPreferences(CUBalanceSettings.PREFS_NAME, MODE_PRIVATE);
+    
+    String prefsUser = settings.getString(CUBalanceSettings.USER_KEY, "");
+    String prefsPin  = settings.getString(CUBalanceSettings.PIN_KEY,  "");
 
-		final SharedPreferences settings = getSharedPreferences(CUBalanceSettings.PREFS_NAME, MODE_PRIVATE);
+    Log.i(TAG, "Spawning a CUBalanceFetcher task.");
+    
+    try {
+      new CUBalanceFetcher(prefsUser, prefsPin, mainUI).execute();
+      Toast.makeText(this, "Updating Balance...", Toast.LENGTH_SHORT).show();
+    } catch(Exception e) {
+      Log.e(TAG, "Unable to instantiate CUBalanceFetcher");
+      Log.e(TAG, e.getMessage());
+      Log.e(TAG, e.toString());
+      
+      Toast.makeText(this, "Error updating balance.", Toast.LENGTH_SHORT).show();
+    }
+  }
 
-		final TextView balance = (TextView) findViewById(R.id.balanceTxt);
-		final TextView updatedAt = (TextView) findViewById(R.id.updatedAtTxt);
+  public void updateBalance(CUBalanceResult result)
+  {
+    final TextView  balance   = (TextView) findViewById(R.id.balanceTxt);
+    final TextView  updatedAt = (TextView) findViewById(R.id.updatedAtTxt);
+    
+    if(result.hasError())
+    {
+      Toast.makeText(this, result.getError(), Toast.LENGTH_SHORT).show();
+      return;
+    }
 
-		Boolean autoUp = settings.getBoolean(CUBalanceSettings.UPDATE_KEY, false);
-		String prefsUser = settings.getString(CUBalanceSettings.USER_KEY, "");
-		String prefsPin = settings.getString(CUBalanceSettings.PIN_KEY, "");
-		String lastBal = settings.getString(CUBalanceSettings.BAL_KEY, "");
-		String lastUp = settings.getString(CUBalanceSettings.DATE_KEY, "");
+    final SharedPreferences settings 
+      = getSharedPreferences(CUBalanceSettings.PREFS_NAME, MODE_PRIVATE);
+    final SharedPreferences.Editor editor = settings.edit();
+    
+    String dateStr    = DATE_FMT.format(new java.util.Date());
+    String balanceStr = CASH_FMT.format(result.getBalance());
 
-		// If the user or pin haven't been changed from the default, we need to show
-		// the settings screen before anything meaningful can happen in this Activity
-		if (prefsUser.equals("") || prefsPin.equals("")) {
-			Log.i(TAG, "No first setup done. Launching CUBalanceSettings activity.");
-			Intent myIntent = new Intent(getBaseContext(), CUBalanceSettings.class);
-			startActivityForResult(myIntent, 0);
-		}
-
-		// There is a 'cached' balance, set the text box to that value.
-		if (!lastBal.equals("")) {
-			balance.setText(lastBal);
-		}
-
-		if (!lastUp.equals("")) {
-			updatedAt.setText("Last Updated: " + lastUp);
-		}
-
-		if (autoUp) {
-			fetchBalance();
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.menu, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle item selection
-		switch (item.getItemId()) {
-		case R.id.menu_settings_item:
-			Intent myIntent = new Intent(getBaseContext(), CUBalanceSettings.class);
-			startActivityForResult(myIntent, 0);
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
-
-	private void fetchBalance() {
-		final CUCampusCardBalanceActivity mainUI = this;
-
-		final SharedPreferences settings = getSharedPreferences(CUBalanceSettings.PREFS_NAME, MODE_PRIVATE);
-
-		String prefsUser = settings.getString(CUBalanceSettings.USER_KEY, "");
-		String prefsPin = settings.getString(CUBalanceSettings.PIN_KEY, "");
-
-		Log.i(TAG, "Spawning a CUBalanceFetcher task.");
-
-		try {
-			new CUBalanceFetcher(prefsUser, prefsPin, mainUI).execute();
-			Toast.makeText(this, "Updating Balance...", Toast.LENGTH_SHORT).show();
-		} catch (Exception e) {
-			Log.e(TAG, "Unable to instantiate CUBalanceFetcher");
-			Log.e(TAG, e.getMessage());
-			Log.e(TAG, e.toString());
-
-			Toast.makeText(this, "Error updating balance.", Toast.LENGTH_SHORT).show();
-		}
-	}
-
-	public void updateBalance(CUBalanceResult result) {
-		final TextView balance = (TextView) findViewById(R.id.balanceTxt);
-		final TextView updatedAt = (TextView) findViewById(R.id.updatedAtTxt);
-
-		if (result.hasError()) {
-			Toast.makeText(this, result.getError(), Toast.LENGTH_SHORT).show();
-			return;
-		}
-
-		final SharedPreferences settings = getSharedPreferences(CUBalanceSettings.PREFS_NAME, MODE_PRIVATE);
-		final SharedPreferences.Editor editor = settings.edit();
-
-		String dateStr = DATE_FMT.format(new java.util.Date());
-		String balanceStr = CASH_FMT.format(result.getBalance());
-
-		Log.i(TAG, "Updating cached balance to " + balanceStr);
-		Log.i(TAG, "Updating cached balance date to " + dateStr);
-		editor.putString(CUBalanceSettings.BAL_KEY, balanceStr);
-		editor.putString(CUBalanceSettings.DATE_KEY, dateStr);
-		editor.commit();
-
-		// Update widget for new balance
-		ComponentName cn = new ComponentName(this, CUBalanceWidgetProvider.class);
-		RemoteViews views = new RemoteViews(getPackageName(), R.layout.cubalance_widget);
-		views.setTextViewText(R.id.tvWidgetBalance, balanceStr);
-		views.setTextViewText(R.id.tvWidgetLastUp, dateStr);
-		AppWidgetManager awm = AppWidgetManager.getInstance(this);
-		awm.updateAppWidget(cn, views);
-
-		balance.setText(balanceStr);
-		updatedAt.setText("Last Updated: " + dateStr);
-
-		Toast.makeText(this, "Balance updated.", Toast.LENGTH_SHORT).show();
-	}
+    Log.i(TAG, "Updating cached balance to "+ balanceStr);
+    Log.i(TAG, "Updating cached balance date to "+ dateStr);
+    editor.putString(CUBalanceSettings.BAL_KEY, balanceStr);
+    editor.putString(CUBalanceSettings.DATE_KEY, dateStr);
+    editor.commit();
+    
+    balance.setText(balanceStr);
+    updatedAt.setText("Last Updated: "+ dateStr);
+    
+    Toast.makeText(this, "Balance updated.", Toast.LENGTH_SHORT).show();
+  }
 }
